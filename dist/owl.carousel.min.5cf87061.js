7a9c975ffdb83d311f73de450b05c2325c6ecf06
nabaan <nabaa96@DOMAIN.local>
2019-01-14 22:41:05 +0300

    Added a Procfile.

--- dist/owl.carousel.min.5cf87061.js
+++ dist/owl.carousel.min.5cf87061.js
@@ -1158,7 +1158,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60080" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64754" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
